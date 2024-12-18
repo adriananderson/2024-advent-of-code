a08466669d994805639988012c73b8a3237ff682
Adrian Anderson <adriananderson@seed-innovations.com>
2024-12-18 08:17:49 -0700

    binary search tree

--- day18/Part2.go
+++ day18/Part2.go
@@ -8,17 +8,35 @@ import (
 	"strings"
 )
 
-func Part2(fileName string, maxSize int, limit int) string {
+func Part2(fileName string, maxSize int, start int) string {
 	defer utils.Timer("18-2")()
 
 	wallCoordinates := readWallCoordinates(fileName)
 	wallMap := makeMaze(maxSize)
 
-	for ii := limit; ii < len(wallCoordinates); ii++ {
-		loadWallMap(wallCoordinates, wallMap, ii)
-		pathLength := solveMaze(wallMap, maxSize)
-		if pathLength == math.MaxInt {
-			return fmt.Sprintf("%d,%d", wallCoordinates[ii-1][0], wallCoordinates[ii-1][1])
+	low := start
+	high := len(wallCoordinates) - 1
+
+	for low <= high {
+		mid := (low + high) / 2
+
+		//fmt.Printf("low: %d   mid: %d   high: %d\n", low, mid, high)
+
+		loadWallMap(wallCoordinates, wallMap, mid)
+		pathLengthMid := solveMaze(wallMap, maxSize)
+
+		loadWallMap(wallCoordinates, wallMap, mid-1)
+		pathLengthPrevMid := solveMaze(wallMap, maxSize)
+
+		if pathLengthPrevMid < math.MaxInt && pathLengthMid == math.MaxInt {
+			//fmt.Printf("solved %d\n", mid)
+			return fmt.Sprintf("%d,%d", wallCoordinates[mid-1][0], wallCoordinates[mid-1][1])
+		} else if pathLengthPrevMid == math.MaxInt {
+			//fmt.Printf("blocked %d\n", mid)
+			high = mid - 2
+		} else if pathLengthMid < math.MaxInt {
+			//fmt.Printf("safe %d\n", mid)
+			low = mid + 1
 		}
 	}
 
@@ -28,7 +46,7 @@ func Part2(fileName string, maxSize int, limit int) string {
 func readWallCoordinates(fileName string) (wallCoordinates [][]int) {
 	coordLines, _ := utils.ReadFileAsLines(fileName)
 
-	wallCoordinates = make([][]int, len(coordLines))
+	wallCoordinates = make([][]int, len(coordLines)-1)
 	for ii, coordinate := range coordLines {
 		if len(coordinate) > 0 {
 			wallCoordinates[ii] = make([]int, 2)
@@ -56,5 +74,8 @@ func loadWallMap(coordLines [][]int, wallMap [][]bool, lineLimit int) {
 
 	for ii := 0; ii < lineLimit; ii++ {
 		wallMap[coordLines[ii][0]][coordLines[ii][1]] = true
+	}
+	for ii := lineLimit; ii < len(coordLines); ii++ {
+		wallMap[coordLines[ii][0]][coordLines[ii][1]] = false
 	}
 }
